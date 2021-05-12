c11745b941af61f1fa1f7e04ca92deb95ae494d2
Jordan DeDea <jordandedea@gmail.com>
2021-05-12 11:24:44 -0500

    Migrated database and added ambiance field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_06_175847) do
+ActiveRecord::Schema.define(version: 2021_05_12_161539) do
 
   create_table "reviews", force: :cascade do |t|
     t.string "title"
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2021_05_06_175847) do
     t.string "cuisine"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "phone_number"
+    t.string "ambiance"
   end
 
 end
